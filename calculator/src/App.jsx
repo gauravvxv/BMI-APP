a394a2c27cec3dd7bf386af326d22f56104cf5cc
Gaurav Sharma <gauraxvx0410@gmail.com>
2024-07-24 14:57:29 +0530

    functions Done

---
+++ calculator/src/App.jsx
@@ -0,0 +1,44 @@
+import logo from './logo.svg';
+import './App.css';
+import { useMemo, useState } from 'react';
+
+function App() {
+  const[weight,setWeight] = useState(70);
+  const[height,setHeight] = useState(170)
+
+  const handleWeight = (e) => {
+    setWeight(e.target.value);
+  }
+
+  const handleHeight = (e) => {
+    setHeight(e.target.value);
+  }
+
+const output = useMemo(()=>{
+
+  const calculatedHeight = height / 100;
+
+  return (weight / (calculatedHeight*calculatedHeight)).toFixed(2);
+
+},[weight,height])
+  return (
+    <div className="App">
+      <h1>BMI CALCULATOR</h1>
+
+      <div>
+        <div>
+          <p>Weight:{weight}kg</p>
+          <input type="range" step="1" min="40" max="200" onChange={handleWeight} />
+
+          <p>Height:{height}cm</p>
+          <input type="range" min="130" max="220" onChange={handleHeight} />
+           <p>Your BMI is</p>
+           <p>{output}</p>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
